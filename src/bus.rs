e08a971cbb9884a1bd684098b2cfdce72013139e
Vikas Chaurasia <getintouchwithvic@gmail.com>
2026-05-08 21:53:55 +0530

    add: organisation + comments on the bus

--- src/bus.rs
+++ src/bus.rs
@@ -30,23 +30,33 @@ impl Bus {
     pub fn read(&self, addr: u16) -> u8 {
         match addr {
             // Cartridge ROM (fixed)
-            0x0000..=0x7FFF => {
-                //something
-                self.cartridge.read(addr)
-            }
+            0x0000..=0x7FFF => self.cartridge.read(addr),
             0x8000..=0x9FFF => self.vram.read(addr),
+
+            // THIS one is for the Catridge's RAM, which fascinates me further
+            // people were putting RAM in their games. LITERALLY shipping ram with the game.
             0xA000..=0xBFFF => self.cartridge.read(addr),
+
+            //
             0xC000..=0xCFFF => self.wram.read(addr),
+
             // ECHO RAM
             0xE000..=0xFDFF => self.wram.read(addr - 0x2000),
+
+            //
             0xFE00..=0xFE9F => self.oam.read(addr),
+
             // Nintendo forbade accessing this memory, so to be faithful
             0xFEA0..=0xFEFF => {
                 // panic!("thou shall ninetendont");
                 return 0x00;
             }
+
+            // To handle I/O Stuff, keypresses etc
             0xFF00..=0xFF7F => self.io.read(addr),
+
             0xFF80..=0xFFFE => self.hram.read(addr),
+
             0xFFFF..=0xFFFF => self.io.read(addr), // FOR HANDLING INTERRUPTS
 
             _ => panic!("something went wrong!"),
