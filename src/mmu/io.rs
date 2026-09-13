1ac117b9716ebecc8b98c2e81eb5b98515f72112
Vikas Chaurasia <getintouchwithvic@gmail.com>
2026-09-13 18:04:15 +0530

    add: mock implementation of Io module for tests

--- src/mmu/io.rs
+++ src/mmu/io.rs
@@ -1,15 +1,36 @@
 use crate::utils::Memory;
 
-pub struct Io {}
+pub struct Io {
+    sb: u8,         // this 0xFF01
+    sc: u8,         // 0xFF02
+    pub if_reg: u8, //0xFF0F
+}
 
 impl Memory for Io {
     fn read(&self, addr: u16) -> u8 {
-        // TODO:
-        0x00
+        match addr {
+            0xFF01 => self.sb,
+            0xFF02 => self.sc,
+            0xFF0F => self.if_reg,
+            0xFFFF => 0x0, // IE register
+            _ => 0xFF,
+        }
     }
 
     fn write(&mut self, addr: u16, value: u8) {
         // TODO:
         println!("Writing!: {:?}", value);
+        match addr {
+            0xFF01 => self.sb = value,
+            0xFF02 => {
+                self.sc = value;
+                if value == 0x81 {
+                    print!("{}", self.sb as char);
+                    self.sc = 0x00;
+                }
+            }
+            0xFF0F => self.if_reg = value,
+            _ => {}
+        }
     }
 }
